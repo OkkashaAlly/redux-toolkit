c3f14d3e0d429ea9ef1eba3107f3e85274d975f8
OkkashaAlly <steamnature006@gmail.com>
2023-03-27 11:26:50 +0300

    Basic reducer

---
+++ index.js
@@ -0,0 +1,56 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+// action
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+// action creator
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+};
+const restockCake = (qty = 1) => {
+  return {
+    type: 'CAKE_RESTOCKED',
+    payload: qty
+  }
+}
+
+// state
+const initialState = {
+  numberOfCakes: 10,
+};
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - action.payload,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes + action.payload
+      }
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial state: ", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state: ", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(3));
+
+unsubscribe();
