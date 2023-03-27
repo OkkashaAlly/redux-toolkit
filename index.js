9f9fd509c83dcd5a58ce40036d9441ed59352d97
OkkashaAlly <steamnature006@gmail.com>
2023-03-27 11:39:15 +0300

    bindActionCreators

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators
 
 // action
 const CAKE_ORDERED = "CAKE_ORDERED";
@@ -48,9 +49,15 @@ const unsubscribe = store.subscribe(() =>
   console.log("Updated state: ", store.getState())
 );
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockCake(3));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(3));
+const actions = bindActionCreators({orderCake, restockCake}, store.dispatch)
+
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
 
 unsubscribe();
